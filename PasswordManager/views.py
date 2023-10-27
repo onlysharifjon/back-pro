426955e7106e7a5cc6ec3a8bfb69a9e0fc2d9ff3
ummad741 <ummad741@gmail.com>
2023-10-27 15:43:43 +0500

    task

--- PasswordManager/views.py
+++ PasswordManager/views.py
@@ -1,25 +1,32 @@
+import random
+from .models import PasswordModel
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import PasswordGeneratorSerializer
 from rest_framework.response import Response
 
+
 def index(request):
     return render(request, 'index.html')
 
 
-from .models import PasswordModel
-import random
-
 # men
+
 class PasswordLevel1(APIView):
     queryset = PasswordModel.objects.all()
-    serializer = PasswordGeneratorSerializer  # Make sure to replace 'your_app_name' with your actual app name
+    # Make sure to replace 'your_app_name' with your actual app name
+    serializer = PasswordGeneratorSerializer
 
     def get(self, request):
-        elements = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
+        elements = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+                    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
         parolcha = ''
-        for i in range(8):
+
+        for i in range(6):
             parolcha += random.choice(elements)
+        for i in range(2):
+            parolcha += random.randint(0, 9)
+            
 
         return Response({'password': parolcha})
 
